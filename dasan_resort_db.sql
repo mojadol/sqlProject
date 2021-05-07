f527f0260e2c15cc1b48ecb96cee0c770dbc2802
ddore95 <ddore95@ajou.ac.kr>
2021-05-07 22:45:25 +0900

    Update dasan_resort_db.sql

--- dasan_resort_db.sql
+++ dasan_resort_db.sql
@@ -3,7 +3,11 @@ CREATE DATABASE dasan_resort;
 use dasan_resort;
 
 CREATE TABLE SERVICE(
+<<<<<<< Updated upstream
 'Service_ID' INT NOT NULL AUTO_INCREMENT COMMENT '서비스ID',
+=======
+'Service_ID' INT NOT NULL AUTO_INCREMENT COMMENT '서비스 ID',
+>>>>>>> Stashed changes
 'Service_Type' INT NOT NULL COMMENT '서비스 종류',
 'Service_Price' VARCHAR NOT NULL COMMENT '서비스 가격',
 PRIMARY KEY (Service_ID)
@@ -16,12 +20,24 @@ CREATE TABLE RESTAURANT_ORDER(
 'ResOrder_Date' DATETIME NOT NULL COMMENT '레스토랑 주문 날짜',
 'ResOrder_Time' INT NOT NULL COMMENT '레스토랑 주문 시각',
 'RestOrder_TotalAmount[Res_Menu, Res_Price]' INT(15) NOT NULL COMMENT '레스토랑 총 주문 가격'
+<<<<<<< Updated upstream
 'KEY_ID' INT NOT NULL COMMENT '카드키 ID',
 'Res_ID' INT NOT NULL COMMENT '레스토랑 ID',
 'Cust_ID' INT NOT NULL COMMENT ' 고객 ID',
 PRIMARY KEY ('ResOrder_ID','ResOrder_Date')
 FOREIGN KEY (
 );
+=======
+'KEY_ID' INT NOT NULL COMMENT '키 ID',
+'Res_ID' INT NOT NULL COMMENT '레스토랑 ID',
+'Cust_ID' INT NOT NULL COMMENT ' 고객 ID',
+PRIMARY KEY ('ResOrder_ID','ResOrder_Date')
+FOREIGN KEY (KEY_ID) REFERENCES CARD KEY(KEY_ID) ON UPDATE CASCADE ON DELETE CASCADE
+FOREIGN KEY (Res_ID) REFERENCES RESTAURANT(Res_ID) ON UPDATE CASCADE ON DELETE CASCADE
+FOREIGN KEY (Cust_ID) REFERENCES CUSTOMER(Cust_ID) ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+>>>>>>> Stashed changes
 ALTER TABLE RESTAURANT_ORDER COMMENT '레스토랑 주문';
 
 
